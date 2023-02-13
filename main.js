7adce298c5806116866068ade68c0f731ee48dd1
unknown <tombirbeck47@gmail.com>
2023-02-13 11:22:52 +0000

    updated readme, altered styling slightly

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const id = document.querySelector('.advice-id')
 const advice = document.querySelector('.advice')
+const button = document.querySelector('.button')
 
 
 const getQuote = async () => {
     const res = await fetch('https://api.adviceslip.com/advice')
     const data = await res.json()
     id.textContent = data.slip.id
-    advice.textContent = data.slip.advice
+    advice.textContent = '"' + data.slip.advice + '"'
 }
 
 getQuote()
+
+button.addEventListener('click', getQuote)
